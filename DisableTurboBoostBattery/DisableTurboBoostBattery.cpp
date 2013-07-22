e0348e16193909427f2ed4baa60af6d6e62115cb
qwerty12 <faheem@Minitrue.local>
2013-07-22 12:52:52 +0100

    Check to see if a battery is installed before acting
    Thanks to noreye for raising the bug over at OSXLatitude

--- DisableTurboBoostBattery/DisableTurboBoostBattery.cpp
+++ DisableTurboBoostBattery/DisableTurboBoostBattery.cpp
@@ -1,7 +1,5 @@
 //Copyright (C) 2013 qwerty12
 //License: http://www.gnu.org/licenses/gpl.html GPL version 2
-//Note, the GPLv2.html file inside NoSleep's installer suggests it is, in fact, a GPLv2'd project. However, the Google Code project page says it is under the New BSD License.
-//Sticking with the GPLv2 license for now, but will change if it is actually under the BSD license.
 
 //Butchered work pretty much entirely taken from/based on code from:
 //
@@ -21,7 +19,7 @@ OSDefineMetaClassAndStructors(DisableTurboBoostBattery, IOService)
 
 void DisableTurboBoostBattery::startPM(IOService *provider)
 {
-	static const int kMyNumberOfStates = 2;
+	const int kMyNumberOfStates = 2;
 	static IOPMPowerState myPowerStates[kMyNumberOfStates] = {
 		{kIOPMPowerStateVersion1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{kIOPMPowerStateVersion1, kIOPMPowerOn, kIOPMPowerOn, kIOPMPowerOn, 0, 0, 0, 0, 0, 0, 0, 0}
@@ -31,8 +29,7 @@ void DisableTurboBoostBattery::startPM(IOService *provider)
 	provider->joinPMtree(this);
 	registerPowerDriver(this, myPowerStates, kMyNumberOfStates);
 
-	if (OSDictionary *tmpDict = serviceMatching("IOPMPowerSource"))
-	{
+	if (OSDictionary *tmpDict = serviceMatching("IOPMPowerSource")) {
 		addMatchingNotification(gIOFirstPublishNotification, tmpDict,
 								&DisableTurboBoostBattery::_powerSourcePublished,
 								this, this);
@@ -43,7 +40,7 @@ void DisableTurboBoostBattery::startPM(IOService *provider)
 
 void DisableTurboBoostBattery::stopPM()
 {
-	if(powerStateNotifier){
+	if(powerStateNotifier) {
 		powerStateNotifier->remove();
 		powerStateNotifier = NULL;
 	}
@@ -69,16 +66,6 @@ bool DisableTurboBoostBattery::powerSourcePublished(IOService *newService, IONot
 	return true;
 }
 
-IOReturn DisableTurboBoostBattery::setPowerState(unsigned long whichState, __unused IOService * whatDevice)
-{
-	if (whichState == 0) {
-		enable_tb();
-		isOnAC = !isOnAC;
-	}
-
-	return kIOPMAckImplied;
-}
-
 IOReturn DisableTurboBoostBattery::_powerSourceStateChanged(void * target, __unused void * refCon,
 							    UInt32 messageType, IOService * provider,
 							    void * messageArgument, vm_size_t argSize)
@@ -98,18 +85,23 @@ IOReturn DisableTurboBoostBattery::powerSourceStateChanged(UInt32 messageType, _
 
 void DisableTurboBoostBattery::actOnChangedPowerState()
 {
-	if(pPowerSource && isOnAC != pPowerSource->externalChargeCapable()) {
-		isOnAC = pPowerSource->externalChargeCapable();
-		startOrStop();
+	if(pPowerSource && isOnAC != pPowerSource->externalChargeCapable() && pPowerSource->batteryInstalled()) {
+		if ((isOnAC = pPowerSource->externalChargeCapable()))
+			enable_tb();
+		else
+			disable_tb();
 	}
 }
 
-void DisableTurboBoostBattery::startOrStop()
+IOReturn DisableTurboBoostBattery::setPowerState(unsigned long whichState, __unused IOService * whatDevice)
 {
-	if (isOnAC)
+	// Workaround bug with ASUS laptops that enable Turbo Boost again when resuming from sleep by enabling it ourselves before going to sleep and getting the kext to re-apply the correct state
+	if (whichState == 0) {
 		enable_tb();
-	else
-		disable_tb();
+		isOnAC = !isOnAC;
+	}
+
+	return kIOPMAckImplied;
 }
 
 bool DisableTurboBoostBattery::start(IOService *provider)
@@ -118,8 +110,6 @@ bool DisableTurboBoostBattery::start(IOService *provider)
 		return false;
 
 	isOnAC = true;
-	enable_tb();
-
 	startPM(provider);
 
 	return true;
